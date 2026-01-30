28f22b14c13c53913cfc53acab8032e45a65eaec
brandon <b.charest019@gmail.com>
2026-01-30 13:32:34 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
+use std::net:TcpStream;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,8 +10,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+            Ok(mut stream) => {
+                stream.Write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
